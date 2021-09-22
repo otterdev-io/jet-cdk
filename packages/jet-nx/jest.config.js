56928009cc59815b10886040ebc00ac1f9b629c1
Chris Fraser <cfraz89@gmail.com>
2021-09-22 09:52:29 +0800

    rename nx plugin

--- packages/jet-nx/jest.config.js
+++ packages/jet-nx/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'nx-plugin',
+  displayName: 'jet-nx',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/nx-plugin',
+  coverageDirectory: '../../coverage/packages/jet-nx',
 };
--- packages/nx-plugin/README.md
+++
@@ -1,7 +0,0 @@
-# nx-plugin
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test nx-plugin` to execute the unit tests via [Jest](https://jestjs.io).
