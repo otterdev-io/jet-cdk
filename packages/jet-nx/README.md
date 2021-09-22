fe945b63bc9e3bbb6247d8d1ed5e3e3797adc404
Chris Fraser <cfraz89@gmail.com>
2021-09-22 09:52:29 +0800

    rename nx plugin

---
+++ packages/jet-nx/README.md
@@ -0,0 +1,7 @@
+# jet-nx
+
+This library was generated with [Nx](https://jet-nx.dev).
+
+## Running unit tests
+
+Run `jet-nx test jet-nx` to execute the unit tests via [Jest](https://jestjs.io).
--- packages/jet-nx/src/generators/jet-cdk-app/generator.ts
+++ packages/jet-nx/src/generators/jet-cdk-app/generator.ts
@@ -1,12 +1,12 @@
 import { Tree } from '@nrwl/devkit';
 import { CdkAppWithJetGeneratorSchema } from './schema';
-import appGenerator from '@otterdev/nx-cdk/src/generators/app/generator';
+import { cdkAppGenerator } from '@otterdev/nx-cdk/';
 import addToCdkAppGenerator from '../add-to-cdk-app/generator';
 
 export default async function (
   tree: Tree,
   options: CdkAppWithJetGeneratorSchema
 ) {
-  await appGenerator(tree, options);
+  await cdkAppGenerator(tree, options);
   await addToCdkAppGenerator(tree, { project: options.name });
 }
